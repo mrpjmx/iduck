7083bd6cceb7798c69af255f19dd41caffc4c717
pjm <pjimmy@qq.com>
2026-05-07 08:13:03 +0800

    fix: 修复 Decap CMS 登录后跳转问题
    - 在 admin/index.html 添加登录后跳转脚本
    - 添加 netlify-identity.js 处理全局跳转
    - 通过 customJS 引入跳转脚本

---
+++ static/js/netlify-identity.js
@@ -0,0 +1,10 @@
+// Netlify Identity redirect
+if (window.netlifyIdentity) {
+  window.netlifyIdentity.on("init", user => {
+    if (!user) {
+      window.netlifyIdentity.on("login", () => {
+        document.location.href = "/admin/";
+      });
+    }
+  });
+}
